5b9d87ff8a822fe7128ddc82f9fbc629e14aee66
hmjag07 <harshjagtap153@gmail.com>
2025-05-02 07:48:08 +0530

    .

--- Frontend/src/Services/firebase.js
+++ Frontend/src/Services/firebase.js
@@ -1,12 +1,15 @@
 import { initializeApp } from 'firebase/app';
-import { getDatabase, ref, onValue } from 'firebase/database';
 
-const firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG);
-const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
-
-export const subscribeFeeders = (userId, callback) => {
-  const feedersRef = ref(db, `feeders/${userId}`);
-  return onValue(feedersRef, snapshot => callback(snapshot.val()));
+const firebaseConfig = {
+  apiKey: "AIzaSyB8GDv_XO3xfP4vCmbkl4v2uFcfHxU14qY",
+  authDomain: "automatedpetfeeder-4585e.firebaseapp.com",
+  projectId: "automatedpetfeeder-4585e",
+  storageBucket: "automatedpetfeeder-4585e.firebasestorage.app",
+  messagingSenderId: "599324217555",
+  appId: "1:599324217555:web:37669f142626c7f1e20426",
+  measurementId: "G-B3L6CXHJ1L"
 };
+
+const app = initializeApp(firebaseConfig);
+export default app;
 
